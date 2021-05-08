41dd0c28ab1017d25eb1421be2f10c918b11ba64
FerriteGiant <cwgreenley@gmail.com>
2021-05-07 21:28:09 -0700

    Add ability to run in test mode

--- anki_alert_script.py
+++ anki_alert_script.py
@@ -1,5 +1,6 @@
 from datetime import datetime as dt
 import os 
+import sys
 
 from bs4 import BeautifulSoup as bs
 from requests import Session,post
@@ -48,8 +49,10 @@ def load_config_params(file_name: str) -> dict:
   return params
 
 
-def scrape_ankiweb(params: dict) -> bytes:
-  """Login to ankiweb and grab the ankiweb.net/decks page."""
+def scrape_ankiweb(params: dict, test_mode: bool) -> bytes:
+  """Login to ankiweb and grab the ankiweb.net/decks page.
+     In test mode, use a local copy of the webpage
+  """
 
   ANKI_LOGIN_FORM_POST_URL = 'https://ankiweb.net/account/login'
   ANKI_DECKS_URL = 'https://ankiweb.net/decks/'
@@ -61,34 +64,38 @@ def scrape_ankiweb(params: dict) -> bytes:
     'submitted':'1',
     'csrf_token': ''
   }
-  
-  with Session() as s:
-    try:
-      login_page = s.get(ANKI_LOGIN_FORM_POST_URL)
-      bs_login_page = bs(login_page.content, 'html.parser')
-    except: 
-      log_error("Issue accessing anki login page")
 
-    try:
-      token = bs_login_page.find('input',{'name':'csrf_token'})['value']
-      anki_login_payload['csrf_token']=token
-    except (KeyError, TypeError):
-      log_error("Unabled to find csrf token")
-      
-    post_response = s.post(ANKI_LOGIN_FORM_POST_URL, data=anki_login_payload)
-    decks_page = s.get(ANKI_DECKS_URL)
-    if (decks_page.status_code != 200) or ("decks" not in decks_page.url):
-      log_error("Failed to login to ankiweb")
+  if not test_mode:
+    with Session() as s:
+      try:
+        login_page = s.get(ANKI_LOGIN_FORM_POST_URL)
+        bs_login_page = bs(login_page.content, 'html.parser')
+      except: 
+        log_error("Issue accessing anki login page")
 
-    content = decks_page.content
+      try:
+        token = bs_login_page.find('input',{'name':'csrf_token'})['value']
+        anki_login_payload['csrf_token']=token
+      except (KeyError, TypeError):
+        log_error("Unabled to find csrf token")
+        
+      post_response = s.post(ANKI_LOGIN_FORM_POST_URL, data=anki_login_payload)
+      decks_page = s.get(ANKI_DECKS_URL)
+      if (decks_page.status_code != 200) or ("decks" not in decks_page.url):
+        log_error("Failed to login to ankiweb (Status code: {} URL: {}".format(decks_page.status_code,decks_page.url))
+
+      content = decks_page.content
 
 ### Use to create local file for testing
 #    with open("test.html",'wb') as file:
 #      file.write(content)
 
-### Use instead of above code to test everything except logging into and parsing ankiweb
-#  with open("test.html","rb") as f:
-#    content = f.read()
+  else: # Return a local copy of the webpage so the rest of the script can be tested
+    try:
+      with open("test.html","rb") as f:
+        content = f.read()
+    except IOError:
+      log_error("Issue opening or reading from test file")
 
   return content
 
@@ -109,6 +116,7 @@ def munge_decks_page(decks_page_content: bytes) -> int:
 
   return reviews_due
 
+
 def send_alert(params: dict, reviews_due: int) -> None:
   """If appropraite, trigger the IFTTT webhooks endpoint."""
 
@@ -122,8 +130,13 @@ def send_alert(params: dict, reviews_due: int) -> None:
 
 
 def main():
+  args = sys.argv[1:]
+  test_mode = False
+  if len(args)==1 and args[0]=='test':
+    test_mode=True
+
   params = load_config_params(PARAMS_FILE)
-  decks_page_content = scrape_ankiweb(params)
+  decks_page_content = scrape_ankiweb(params,test_mode)
   reviews_due = munge_decks_page(decks_page_content)
   send_alert(params, reviews_due)
   log_success(reviews_due)
